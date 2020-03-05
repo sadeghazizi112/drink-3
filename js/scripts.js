5a00fefac67373c317d1cae89f57e8196585001d
sadegh azizi <sadeghazizi112@yahoo.com>
2020-03-05 15:55:30 -0800

    first one

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+js/scripts.js
+$(document).ready(function() {
+  var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+
+  if (over21) {
+    $('#drinks').show();
+  } else {
+    $('#under-21').show();
+  }
+});
